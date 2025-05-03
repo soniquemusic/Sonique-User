db9ed60ec804ed9913be7904f939432aec7e3313
soniquemusic <soniquemusic07@gmail.com>
2025-05-03 16:02:06 +0530

    Chnage navbar

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -207,7 +207,7 @@ const Navbar = () => {
 
       {/* Logout Confirmation Modal */}
       {showLogoutConfirm && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+        <div className="fixed inset-0 bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-neutral-800 rounded-lg p-6 max-w-sm w-full mx-4">
             <h3 className="text-lg font-medium text-white mb-4">Confirm Logout</h3>
             <p className="text-gray-300 mb-6">Are you sure you want to logout?</p>
